c691474dbccd8f813df70b490cd8f54578345c07
Phineas Freak <precise.timing.labs@gmail.com>
2019-02-06 11:35:01 +0200

    Recompile for KSP 1.6.X

--- Source/SaturableRW/Properties/AssemblyInfo.cs
+++ Source/SaturableRW/Properties/AssemblyInfo.cs
@@ -1,11 +1,10 @@
-using System;
 using System.Reflection;
 
 //  General Information about the specified assembly.
 
 [assembly: AssemblyTitle ("SaturableRW")]
 [assembly: AssemblyDescription ("Saturable RW for KSP")]
-[assembly: AssemblyCopyright ("Copyright © 2015 - 2018, Crzyrndm")]
+[assembly: AssemblyCopyright ("Copyright © 2015 - 2019, Crzyrndm")]
 [assembly: AssemblyConfiguration ("Release")]
 [assembly: AssemblyProduct ("SaturableRW")]
 [assembly: AssemblyTrademark ("")]
@@ -18,9 +17,9 @@ using System.Reflection;
 //    • Build Number
 //    • Revision
 
-[assembly: AssemblyVersion ("1.5.1.2335")]
-[assembly: AssemblyFileVersion ("1.5.1.1")]
-[assembly: AssemblyInformationalVersion ("1.5.1.2335")]
+[assembly: AssemblyVersion ("1.6.1.2401")]
+[assembly: AssemblyFileVersion ("1.6.1.1")]
+[assembly: AssemblyInformationalVersion ("1.6.1.2401")]
 
 //  The KSPAssembly attribute can be used to ensure that the plugin assemblies
 //  are loaded in the correct order.
